9c0d65f1f3c6732f29b2cbc4eb3e53fb19b1d65c
Phil Crosby <phil.crosby@gmail.com>
2010-08-27 20:21:20 -0700

    A board which renders the game surface

---
+++ board.js
@@ -0,0 +1,34 @@
+function extend(hashA, hashB) {
+  for (var key in hashB)
+    hashA[key] = hashB[key];
+  return hashA;
+}
+
+/*
+ * The board renders the state of the game, including all HTML effects.
+ */
+function Board(boardElement) {
+  console.log(boardElement);
+  this.divsize = 20;
+  this.boardElement = boardElement
+  this.divs = this.createDivs(20, 20, this.boardElement);
+}
+
+extend(Board.prototype, {
+  /* Creates and returns a widthxheight matrix of divs representing the game's surface. */
+  createDivs: function(width, height, boardElement) {
+    var divs = [];
+    for (var x = 0; x < width; x++) {
+      divs[x] = [];
+      for (var y = 0; y < height; y++) {
+        var div = document.createElement("div");
+        div.className = "cell";
+        div.style.left = (this.divsize * x) + "px";
+        div.style.top = (this.divsize * y) + "px";
+        boardElement.appendChild(div);
+        divs[x][y] = div;
+      }
+    }
+    return divs;
+  }
+});
