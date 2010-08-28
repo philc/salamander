f1bcbe9569273cfcd37ef219f09d0781623e6a89
harry <harry@ooyala.com>
2010-08-27 23:45:06 -0700

    Adding turn processing and basic timers.

--- engine/engine.js
+++ engine/engine.js
@@ -26,6 +26,7 @@ Board.prototype = {
 
   // Cell is object with fields: type, snakeId, orientation(?)
   get: function(x, y) {
+    console.log(this,x,y);
     return this.matrix[x][y];
   },
 
@@ -41,21 +42,32 @@ Board.prototype = {
 function Snake() { this.init(); }
 Snake.prototype = {
   init: function() {
+    this.snakeId = null;
     this.articulations = [];
     this.size = 1;
     this.desiredSize = this.size;
     this.requestedMove = null;
   },
 
-  computeDirection: function() {
-    
+  // Returns direction as a pair of x,y deltas (e.g. [-1,0]).
+  computeHeadDirection: function() {
+    return GridUtils.computeDirection(this.articulations[0], this.articulations[1]);
+  },
+
+  computeTailDirection: function() {
+    var l = this.articulations.length
+    return GridUtils.computeDirection(this.articulations[l-2], this.articulations[l-1]);
+  },
+
+  eatApple: function() {
+    // TODO
   }
 };
 
 
 // Default values
-BOARD_WIDTH = 3;
-BOARD_HEIGHT = 3;
+BOARD_WIDTH = 20;
+BOARD_HEIGHT = 20;
 DESIRED_APPLES = 3;
 
 function Engine(renderedBoard) { this.init(renderedBoard); }
@@ -90,25 +102,79 @@ Engine.prototype = {
       this.board.set(x, y, {type: SNAKE, snakeId: snakeId});
       snake.size += 1;
     }.bind(this));
+    snake.snakeId = snakeId;
     snake.articulations = [[headX, headY], [tailX, tailY]];
     snake.desiredSize = snake.size;
     this.snakes.push(snake);
   },
 
   start: function() {
-    // TODO start timer
+    setTimeout(function() {
+      this.processTurn();
+      this.start();
+    }.bind(this), 1000);
   },
 
   processTurn: function() {
     for (var i = 0; i < this.snakes.length; i++) {
+      var snake = this.snakes[i];
       // Move snake's head
+      var headDirection = snake.requestedMove || snake.computeHeadDirection();
+      var newHead = [snake.articulations[0][0] + headDirection[0],
+                     snake.articulations[0][1] + headDirection[1]];
+      console.log("processTurn", snake, newHead, headDirection);
+      if (GridUtils.outOfBounds(newHead, BOARD_WIDTH, BOARD_HEIGHT)) {
+        // TODO Kill the snake!
+        continue;
+      }
+      var cell = this.board.get(newHead[0], newHead[1]);
+      if (cell.type == SNAKE) {
+        // TODO Kill the snake!
+        continue;
+      }
+      else {
+        if (cell.type == APPLE)
+          snake.eatApple();
+        this.board.set(newHead[0], newHead[1], { type: SNAKE, snakeId: snake.snakeId });
+        if (snake.requestedMove) { // The snake has turned
+          snake.articulations = [newHead].concat(snake.articulations);
+        }
+        else { // The snake is going straight
+          snake.articulations[0] = newHead;
+        }
+      }
+      snake.requestedMove = null;
+
       // Move snake's tail
+      if (snake.desiredSize > snake.size) {
+        snake.size += 1;
+      }
+      else {
+        var l = snake.articulations.length;
+        var oldTail = snake.articulations[l-1];
+        this.board.set(oldTail[0], oldTail[1], { type: EMPTY });
+        var tailDirection = snake.computeTailDirection();
+        var newTail = [oldTail[0] + tailDirection[0],
+                       oldTail[1] + tailDirection[1]];
+        if (newTail[0] == snake.articulations[l-2][0] && newTail[1] == snake.articulations[l-2][1]) {
+          // Remove the last articulation
+          snake.articulations.splice(l-1, 1);
+        }
+        else {
+          snake.articulations[l-1] = newTail;
+        }
+      }
     }
-    this.board.addApples(DESIRED_APPLES - this.board.totalApples);
+    this.addApples(DESIRED_APPLES - this.board.totalApples);
   }
 };
 
 var GridUtils = {
+  outOfBounds: function(point, width, height) {
+    return point[0] < 0 || point[0] >= width ||
+           point[1] < 0 || point[1] >= height;
+  },
+
   iterateAlongLine: function(x1, y1, x2, y2, block) {
     if (x1 == x2)
       this.iterateBetween(y1, y2, function(y) {
@@ -116,7 +182,6 @@ var GridUtils = {
       });
     else if (y1 == y2)
       this.iterateBetween(x1, x2, function(x) {
-        console.log("calling block");
         block(x, y1);
       });
     else
@@ -130,5 +195,11 @@ var GridUtils = {
     else
       for (var i = i1; i >= i2; i--)
         block(i);
+  },
+
+  computeDirection: function(endPoint, startPoint) {
+    var dx = endPoint[0] - startPoint[0];
+    var dy = endPoint[1] - startPoint[1];
+    return [dx == 0 ? dx : dx / Math.abs(dx), dy == 0 ? dy : dy / Math.abs(dy)];
   }
 };
