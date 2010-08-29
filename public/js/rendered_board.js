5c7c47a4cd60dae02b393080c81e464fa084f312
Phil Crosby <phil.crosby@gmail.com>
2010-08-28 18:30:22 -0700

    Add a graphic for food.

--- public/js/rendered_board.js
+++ public/js/rendered_board.js
@@ -5,7 +5,7 @@ function RenderedBoard(width, height, cellSize, boardElement) {
   this.cellSize = cellSize;
   this.boardElement = $(boardElement);
   this.divs = this.createDivs(width, height, this.boardElement);
-  this.TYPE_TO_CLASS = ["empty", "apple", "snake", "snakeHead"];
+  this.TYPE_TO_CLASS = ["empty", "food", "snake", "snakeHead"];
 }
 
 extend(RenderedBoard.prototype, {
